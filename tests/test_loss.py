6e601660307e6ccc2c95558b741c780760eeb70c
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-02-18 20:17:17 +0800

    chore: add boilerplate test code

---
+++ tests/test_loss.py
@@ -0,0 +1,8 @@
+from snnl.loss import pairwise_euclidean_distance
+from snnl.loss import pairwise_cosine_distance
+
+def test_pairwise_euclidean_distance():
+    pass
+
+def test_pairwise_cosine_distance():
+    pass
