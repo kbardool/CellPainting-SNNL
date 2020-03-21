0befb4dad2cb0f43937da72b5b2977e649b1ed1f
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-03-21 23:54:24 +0800

    chore: add optimizer and criterion

--- snnl/models/autoencoder.py
+++ snnl/models/autoencoder.py
@@ -39,6 +39,8 @@ class Autoencoder(torch.nn.Module):
                 torch.nn.Linear(in_features=500, out_features=kwargs["input_shape"]),
             ]
         )
+        self.optimizer = torch.optim.Adam(params=self.parameters(), lr=kwargs["learning_rate"])
+        self.criterion = torch.nn.CrossEntropyLoss()
 
     def forward(self, features):
         activations = {}
