6d4feb1a4c7ce962a0cc27b72b840e850823644e
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-03-28 21:58:24 +0800

    chore: add argparse, device switching, and random seed

--- examples/cnn_classifier.py
+++ examples/cnn_classifier.py
@@ -14,6 +14,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """Sample module for using CNN classifier with SNNL"""
+import argparse
+import torch
+
 from snnl.models.cnn import CNN
 from snnl.utils.data import create_dataloader, load_dataset
 from snnl.utils.metrics import accuracy
@@ -22,12 +25,41 @@ __author__ = "Abien Fred Agarap"
 __version__ = "1.0.0"
 
 
-def main():
+def parse_args():
+    parser = argparse.ArgumentParser(description="Benchmark for Baseline Autoencoder")
+    group = parser.add_argument_group("Parameters")
+    group.add_argument(
+        "-s",
+        "--seed",
+        required=False,
+        default=1234,
+        type=int,
+        help="the random seed value to use, default: [1234]",
+    )
+    group.add_argument(
+        "-d",
+        "--device",
+        required=False,
+        default="cpu",
+        type=str,
+        help="the device to use, default: [cpu]",
+    )
+    arguments = parser.parse_args()
+    return arguments
+
+
+def main(args):
     input_dim = 1
     num_classes = 10
     learning_rate = 1e-2
     batch_size = 512
     epochs = 40
+
+    if args.device == "gpu" or args.device == "cuda":
+        torch.manual_seed(args.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = True
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     train_dataset, test_dataset = load_dataset(name="mnist")
     train_loader = create_dataloader(dataset=train_dataset, batch_size=batch_size)
@@ -36,10 +68,12 @@ def main():
     model = CNN(
         input_dim=input_dim, num_classes=num_classes, learning_rate=learning_rate
     )
+    model = model.to(device)
     model.fit(data_loader=train_loader, epochs=epochs, use_snnl=True, factor=10)
     acc = accuracy(model, test_loader)
     print(f"accuracy: {acc * 100.}%")
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    main(args)
