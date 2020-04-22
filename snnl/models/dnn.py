1a0c14c7b5d987d7ffae7d2e6ea1a18d3e2cfc2c
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-04-22 14:54:05 +0800

    docs: add use_snnl, factor, and temperature to docstring in DNN

--- snnl/models/dnn.py
+++ snnl/models/dnn.py
@@ -93,6 +93,13 @@ class DNN(torch.nn.Module):
             The data loader object that consists of the data pipeline.
         epochs : int
             The number of epochs to train the model.
+        use_snnl : bool
+            Whether to use soft nearest neighbor loss or not. Default: [False].
+        factor : float
+            The soft nearest neighbor loss scaling factor.
+        temperature : int
+            The temperature to use for soft nearest neighbor loss.
+            If None, annealing temperature will be used.
         """
         self.to(self.model_device)
 
@@ -160,6 +167,13 @@ def epoch_train(
         The model to train.
     data_loader : torch.utils.dataloader.DataLoader
         The data loader object that consists of the data pipeline.
+    use_snnl : bool
+        Whether to use soft nearest neighbor loss or not. Default: [False].
+    factor : float
+        The soft nearest neighbor loss scaling factor.
+    temperature : int
+        The temperature to use for soft nearest neighbor loss.
+        If None, annealing temperature will be used.
 
     Returns
     -------
