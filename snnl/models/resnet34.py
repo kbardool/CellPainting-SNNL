2489a4a3d4e842a6cf2589b1c150595076ad0bf3
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-04-26 14:33:26 +0800

    refactor: add correction on resnet34 module header

--- snnl/models/resnet34.py
+++ snnl/models/resnet34.py
@@ -13,7 +13,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""Implementation of ResNet18 model"""
+"""Implementation of ResNet34 model"""
 import torch
 import torchvision
 
