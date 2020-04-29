c6b1dd6e0d6d06b638dcd1d422e5c9e65eae08ef
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-04-29 17:35:02 +0800

    chore: parameterize data_folder for load_dataset()

--- snnl/utils/data.py
+++ snnl/utils/data.py
@@ -23,11 +23,13 @@ __author__ = "Abien Fred Agarap"
 __version__ = "1.0.0"
 
 
-def load_dataset(name: str = "mnist") -> Tuple[object, object]:
+def load_dataset(
+    name: str = "mnist", data_folder: str = "~/torch_datasets"
+) -> Tuple[object, object]:
     """
     Returns a tuple of torchvision dataset objects.
 
-    Parameter
+    Parameters
     ---------
     name : str
         The name of the dataset to load. Current choices:
@@ -36,6 +38,8 @@ def load_dataset(name: str = "mnist") -> Tuple[object, object]:
             3. emnist (EMNIST/Balanced)
             4. cifar10 (CIFAR10)
             5. svhn (SVHN)
+    data_folder : str
+        The path to the folder for the datasets.
 
     Returns
     -------
@@ -47,28 +51,28 @@ def load_dataset(name: str = "mnist") -> Tuple[object, object]:
 
     if name == "mnist":
         train_dataset = torchvision.datasets.MNIST(
-            root="~/torch_datasets", train=True, download=True, transform=transform
+            root=data_folder, train=True, download=True, transform=transform
         )
         test_dataset = torchvision.datasets.MNIST(
-            root="~/torch_datasets", train=False, download=True, transform=transform
+            root=data_folder, train=False, download=True, transform=transform
         )
     elif name == "fashion_mnist":
         train_dataset = torchvision.datasets.FashionMNIST(
-            root="~/torch_datasets", train=True, download=True, transform=transform
+            root=data_folder, train=True, download=True, transform=transform
         )
         test_dataset = torchvision.datasets.FashionMNIST(
-            root="~/torch_datasets", train=False, download=True, transform=transform
+            root=data_folder, train=False, download=True, transform=transform
         )
     elif name == "emnist":
         train_dataset = torchvision.datasets.EMNIST(
-            root="~/torch_datasets",
+            root=data_folder,
             train=True,
             split="balanced",
             download=True,
             transform=transform,
         )
         test_dataset = torchvision.datasets.EMNIST(
-            root="~/torch_datasets",
+            root=data_folder,
             train=False,
             split="balanced",
             download=True,
@@ -76,17 +80,17 @@ def load_dataset(name: str = "mnist") -> Tuple[object, object]:
         )
     elif name == "cifar10":
         train_dataset = torchvision.datasets.CIFAR10(
-            root="~/torch_datasets", train=True, download=True, transform=transform
+            root=data_folder, train=True, download=True, transform=transform
         )
         test_dataset = torchvision.datasets.CIFAR10(
-            root="~/torch_datasets", train=False, download=True, transform=transform
+            root=data_folder, train=False, download=True, transform=transform
         )
     elif name == "svhn":
         train_dataset = torchvision.datasets.SVHN(
-            root="~/torch_datasets", split="train", download=True, transform=transform
+            root=data_folder, split="train", download=True, transform=transform
         )
         test_dataset = torchvision.datasets.SVHN(
-            root="~/torch_datasets", split="test", download=True, transform=transform
+            root=data_folder, split="test", download=True, transform=transform
         )
     return (train_dataset, test_dataset)
 
