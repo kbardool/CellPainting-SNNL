8fc6a80bf4e2e61d6d3d85720a7b5407a6d75049
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-05-12 14:39:08 +0800

    chore: add test on pairwise cosine distance computation

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -15,10 +15,20 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """Test module"""
 import torch
+from snnl import compute_pairwise_distance
 from snnl import pairwise_euclidean_distance
 from snnl import pairwise_cosine_distance
 
 torch.manual_seed(42)
+
+
+def test_compute_pairwise_distance():
+    a = torch.rand((4, 2))
+    b = torch.rand((4, 2))
+    temperature = 10
+    pairwise_distance = compute_pairwise_distance(a, b, temperature, True)
+    assert pairwise_distance.size() == (4, 4)
+    assert type(pairwise_distance) is torch.Tensor
 
 
 def test_pairwise_euclidean_distance():
