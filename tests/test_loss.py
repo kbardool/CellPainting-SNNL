82e3d69dd68854b374d8b161ee149bd421a23d8a
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-05-14 22:14:43 +0800

    chore: add test on probabilistic sampling

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -19,8 +19,19 @@ from snnl import compute_pairwise_distance
 from snnl import same_label_mask
 from snnl import pairwise_euclidean_distance
 from snnl import pairwise_cosine_distance
+from snnl import pick_probability
+
 
 torch.manual_seed(42)
+
+
+def test_pick_probability():
+    a = torch.rand((4, 2))
+    probability_matrix = pick_probability(
+        features=a, temperature=10, cosine_distance=True
+    )
+    assert probability_matrix.size() == (4, 4)
+    assert isinstance(probability_matrix, torch.Tensor)
 
 
 def test_same_label_mask():
