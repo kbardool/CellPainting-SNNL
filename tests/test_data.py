6994983b5efd1497fbabf88a7abe22f957b740dc
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-05-20 02:00:39 +0800

    chore: add test for creating data loader objects

--- tests/test_data.py
+++ tests/test_data.py
@@ -15,3 +15,25 @@ def test_load_data(name):
     assert isinstance(test_data.targets, torch.Tensor)
     assert train_data.data.shape[1:] == (28, 28)
     assert test_data.data.shape[1:] == (28, 28)
+
+
+@pytest.mark.parametrize("name", ["mnist", "fashion_mnist", "emnist"])
+def test_create_dataloader(name):
+    batch_size = 256
+    train_data, test_data = load_dataset(name)
+    train_loader = create_dataloader(train_data, batch_size=batch_size)
+    test_loader = create_dataloader(test_data, batch_size=batch_size)
+    assert isinstance(train_loader, torch.utils.data.DataLoader)
+    assert isinstance(test_loader, torch.utils.data.DataLoader)
+
+    for batch_features, batch_labels in train_loader:
+        assert isinstance(batch_features, torch.Tensor)
+        assert isinstance(batch_labels, torch.Tensor)
+        assert batch_features.size() == (256, 1, 28, 28)
+        break
+
+    for batch_features, batch_labels in test_loader:
+        assert isinstance(batch_features, torch.Tensor)
+        assert isinstance(batch_labels, torch.Tensor)
+        assert batch_features.size() == (256, 1, 28, 28)
+        break
