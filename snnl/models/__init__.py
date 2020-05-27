7087ca1ae2d5b6dd8bb1cbb602adffff29149d91
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-05-27 14:26:26 +0800

    chore: add resnet34 child class

--- snnl/models/__init__.py
+++ snnl/models/__init__.py
@@ -897,3 +897,24 @@ class ResNet18(ResNet):
 
     def forward(self, features):
         return self.resnet.forward(features)
+
+
+class ResNet34(ResNet):
+    def __init__(
+        self,
+        num_classes: int,
+        learning_rate: float,
+        device: torch.device = torch.device("cpu"),
+    ):
+        super().__init__(
+            num_classes=num_classes, learning_rate=learning_rate, device=device
+        )
+        self.device = device
+        self.resnet = torchvision.models.resnet.resnet34(pretrained=True)
+        self.resnet.fc = torch.nn.Linear(
+            in_features=self.resnet.fc.in_features, out_features=num_classes
+        )
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+
+    def forward(self, features):
+        return self.resnet.forward(features)
