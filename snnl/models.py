0879d0f7b24554c68faaa6f80b99a10fa6c16203
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-10-03 23:31:20 +0800

    chore: unpack return value of epoch training function

--- snnl/models.py
+++ snnl/models.py
@@ -656,10 +656,13 @@ class CNN(torch.nn.Module):
                         f"\txent loss = {self.train_xent_loss[-1]:.6f}\t|\tsnn loss = {self.train_snn_loss[-1]:.6f}"
                     )
             else:
+                epoch_loss, epoch_accuracy = self.epoch_train(data_loader)
                 self.train_loss.append(epoch_loss)
+                self.train_accuracy.append(epoch_accuracy)
                 if (epoch + 1) % show_every == 0:
+                    print(f"epoch {epoch + 1}/{epochs}")
                     print(
-                        f"epoch {epoch + 1}/{epochs} : mean loss = {self.train_loss[-1]:.6f}"
+                        "mean loss = {self.train_loss[-1]:.6f}\t|\tmean acc = {self.train_accuracy[-1]:.6f}"
                     )
 
     def predict(
