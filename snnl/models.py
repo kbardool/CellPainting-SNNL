2698af077cbf1b699f103628138821dd44327e4c
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-11-24 21:39:13 +0800

    use computed conv layer output shapes

--- snnl/models.py
+++ snnl/models.py
@@ -595,22 +595,33 @@ class CNN(Model):
             temperature=temperature,
             stability_epsilon=stability_epsilon,
         )
+        conv1_out = self.compute_conv_out(input_dim, CNN._conv1_params)
+        conv2_out = self.compute_conv_out(conv1_out, CNN._conv2_params)
         self.layers = torch.nn.ModuleList(
             [
                 torch.nn.Conv2d(
                     in_channels=input_dim,
-                    out_channels=64,
-                    kernel_size=8,
-                    stride=2,
-                    padding=1,
+                    out_channels=CNN._conv1_params.get("out_channels"),
+                    kernel_size=CNN._conv1_params.get("kernel_size"),
+                    stride=CNN._conv1_params.get("stride"),
+                    padding=CNN._conv1_params.get("padding"),
                 ),
                 torch.nn.ReLU(inplace=True),
                 torch.nn.Conv2d(
-                    in_channels=64, out_channels=128, kernel_size=6, stride=2, padding=1
+                    in_channels=CNN._conv1_params.get("out_channels"),
+                    out_channels=CNN._conv2_params.get("out_channels"),
+                    kernel_size=CNN._conv2_params.get("kernel_size"),
+                    stride=CNN._conv2_params.get("stride"),
+                    padding=CNN._conv2_params.get("padding"),
                 ),
                 torch.nn.ReLU(inplace=True),
                 torch.nn.Flatten(),
-                torch.nn.Linear(in_features=(128 * 5 * 5), out_features=1024),
+                torch.nn.Linear(
+                    in_features=int(
+                        CNN._conv2_params.get("out_channels") * conv2_out * conv2_out
+                    ),
+                    out_features=1024,
+                ),
                 torch.nn.ReLU(inplace=True),
                 torch.nn.Linear(in_features=1024, out_features=1024),
                 torch.nn.ReLU(inplace=True),
