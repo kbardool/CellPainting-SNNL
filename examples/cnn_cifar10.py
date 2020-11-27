e65cad34a9180410b19e7f4573680d9c2c601fb9
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-11-27 23:14:16 +0800

    remove base functions for training, epoch training, and inference

--- examples/cnn_cifar10.py
+++ examples/cnn_cifar10.py
@@ -9,18 +9,3 @@ class CNN(torch.nn.Module):
 
     def forward(self, features: torch.Tensor) -> torch.Tensor:
         pass
-
-    def fit(
-        self, data_loader: torch.utils.data.DataLoader, epochs: int, show_every: int = 2
-    ):
-        pass
-
-    def epoch_train(
-        self, data_loader: torch.utils.data.DataLoader, epoch: int = None
-    ) -> Tuple:
-        pass
-
-    def predict(
-        self, features: torch.Tensor
-    ) -> torch.Tensor or Tuple[torch.Tensor, torch.Tensor]:
-        pass
