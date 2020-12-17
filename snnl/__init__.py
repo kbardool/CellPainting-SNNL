ee7f875794f3c8f9a1a794d3ce81fc6c33e16fed
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-12-17 22:18:06 +0800

    remove annealing

--- snnl/__init__.py
+++ snnl/__init__.py
@@ -129,12 +129,6 @@ class SNNLoss(torch.nn.Module):
         snn_loss: float
             The soft nearest neighbor loss value.
         """
-        temperature = (
-            (1.0 / ((1.0 + epoch) ** 0.55))
-            if self.temperature is None
-            else self.temperature
-        )
-
         primary_loss = self.primary_criterion(
             outputs, features if self.unsupervised else labels
         )
@@ -155,7 +149,7 @@ class SNNLoss(torch.nn.Module):
             product = torch.matmul(normalized_a, normalized_b)
             distance_matrix = torch.sub(torch.tensor(1.0), product)
             pairwise_distance_matrix = torch.exp(
-                -(distance_matrix / temperature)
+                -(distance_matrix / self.temperature)
             ) - torch.eye(value.shape[0]).to(model.device)
             pick_probability = pairwise_distance_matrix / (
                 self.stability_epsilon
