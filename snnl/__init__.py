b9e9e2d0c8e621c5b72a904a2ad9d9ed01dcd378
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-07-03 21:34:47 +0800

    call sampling prob function

--- snnl/__init__.py
+++ snnl/__init__.py
@@ -169,9 +169,12 @@ class SNNLoss(torch.nn.Module):
             pairwise_distance_matrix = self.normalize_distance_matrix(
                 features=value, distance_matrix=distance_matrix, device=model.device
             )
-            pick_probability = pairwise_distance_matrix / (
-                self.stability_epsilon
-                + torch.sum(pairwise_distance_matrix, 1).view(-1, 1)
+            # pick_probability = pairwise_distance_matrix / (
+            #     self.stability_epsilon
+            #     + torch.sum(pairwise_distance_matrix, 1).view(-1, 1)
+            # )
+            pick_probability = self.compute_sampling_probability(
+                pairwise_distance_matrix
             )
             masking_matrix = torch.squeeze(
                 torch.eq(labels, labels.unsqueeze(1)).float()
